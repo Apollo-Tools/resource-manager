43483af6150c61331ab9a133ee83778e5bda85e6
matthi-g <matthias.gassner@student.uibk.ac.at>
2022-09-19 16:59:23 +0200

    Add tests for HttpHelper

---
+++ src/test/java/at/uibk/dps/rm/util/HttpHelperTest.java
@@ -0,0 +1,45 @@
+package at.uibk.dps.rm.util;
+
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import io.vertx.rxjava3.core.Vertx;
+import io.vertx.rxjava3.ext.web.RoutingContext;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(VertxExtension.class)
+@ExtendWith(MockitoExtension.class)
+public class HttpHelperTest {
+
+    @Mock
+    RoutingContext rc;
+
+    @Test
+    void getLongPathParamValid(Vertx vertx, VertxTestContext testContext) {
+        doReturn("10").when(rc).pathParam(anyString());
+
+        HttpHelper.getLongPathParam(rc, "id")
+                .subscribe(result -> testContext.verify(() -> {
+                    assertThat(result).isEqualTo(10L);
+                    testContext.completeNow();
+                }));
+    }
+
+    @Test
+    void getLongPathParamInValid(Vertx vertx, VertxTestContext testContext) {
+        doReturn("test").when(rc).pathParam(anyString());
+
+        HttpHelper.getLongPathParam(rc, "id")
+                .subscribe(result -> testContext.verify(() -> fail("method did not throw exception")),
+                        throwable -> testContext.verify(() -> {
+                            assertThat(throwable).isInstanceOf(NumberFormatException.class);
+                            testContext.completeNow();
+                        }));
+    }
+}
