1dd6bee26a9275f119ee5fbc2bb0634ef68818e4
matthi-g <matthias.gassner@student.uibk.ac.at>
2022-11-13 10:44:00 +0100

    Remove unused imports

--- src/main/java/at/uibk/dps/rm/util/ServiceLevelObjectiveDeserializer.java
+++ src/main/java/at/uibk/dps/rm/util/ServiceLevelObjectiveDeserializer.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import io.vertx.core.json.JsonObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
--- src/test/java/at/uibk/dps/rm/testutil/TestObjectProvider.java
+++ src/test/java/at/uibk/dps/rm/testutil/TestObjectProvider.java
@@ -10,7 +10,6 @@ import at.uibk.dps.rm.entity.model.*;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class TestObjectProvider {
 
