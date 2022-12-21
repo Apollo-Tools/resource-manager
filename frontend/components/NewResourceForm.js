ac2c9bdd195e9f07bc89f83a21dd92cd5ad93ef8
matthi-g <matthias.gassner@student.uibk.ac.at>
2022-12-21 11:01:03 +0100

    Create component for new resource form

---
+++ frontend/components/NewResourceForm.js
@@ -0,0 +1,65 @@
+import { Button, Checkbox, Form, Select } from 'antd';
+import { createResource } from '../lib/ResourceService';
+import { useEffect, useState } from 'react';
+import { listResourceTypes } from '../lib/ResourceTypeService';
+
+
+const NewResourceForm = ({ setNewResource, token }) => {
+    const [error, setError] = useState();
+    const [resourceTypes, setResourceTypes] = useState([]);
+
+    useEffect(() => {
+        listResourceTypes(token, setResourceTypes, setError);
+    }, [])
+
+    const onFinish = async (values) => {
+        await createResource(values.resourceType, values.isSelfManaged, token, setNewResource, setError);
+    };
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    return (
+        <div className="card container w-11/12 max-w-7xl p-10">
+            <h2>New Resource</h2>
+            <Form
+                name="basic"
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+            >
+                <Form.Item
+                    label="Is self managed?"
+                    name="isSelfManaged"
+                    valuePropName="checked"
+                >
+                    <Checkbox defaultChecked={false}/>
+                </Form.Item>
+
+                <Form.Item
+                    label="Resource Type"
+                    name="resourceType"
+                >
+                    <Select className="w-40">
+                        {resourceTypes.map(resourceType => {
+                            return (
+                                <Select.Option value={resourceType.type_id} key={resourceType.type_id}>
+                                    {resourceType.resource_type}
+                                </Select.Option>
+                            );
+                        })}
+
+                    </Select>
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        Create
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
+    );
+}
+
+export default NewResourceForm;
