e6041165ed2eb77a1ab7cd6cc5d93de820e99af2
matthi-g <matthias.gassner@student.uibk.ac.at>
2023-01-02 15:16:37 +0100

    Fix codacy issues

--- frontend/lib/AccountService.js
+++ frontend/lib/AccountService.js
@@ -1,21 +1,21 @@
-const API_ROUTE = `${process.env.NEXT_PUBLIC_API_URL}/accounts`
+const API_ROUTE = `${ process.env.NEXT_PUBLIC_API_URL }/accounts`;
 
 export async function login(username, password, setToken, setError) {
-    try {
-        const response = await fetch(`${API_ROUTE}/login`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                username: username,
-                password: password
-            })
-        });
-        const data = await response.json();
-        setToken(data.token);
-    } catch(error) {
-        setError(true);
-        console.log(error)
-    }
+  try {
+    const response = await fetch(`${ API_ROUTE }/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    });
+    const data = await response.json();
+    setToken(data.token);
+  } catch (error) {
+    setError(true);
+    console.log(error);
+  }
 }
