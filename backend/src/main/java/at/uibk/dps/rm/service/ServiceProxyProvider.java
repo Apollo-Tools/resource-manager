da743b7546630d6ff8d7257f21daf881944e1864
matthi-g <matthias.gassner@student.uibk.ac.at>
2023-03-01 16:54:03 +0100

    Register new services

--- backend/src/main/java/at/uibk/dps/rm/service/ServiceProxyProvider.java
+++ backend/src/main/java/at/uibk/dps/rm/service/ServiceProxyProvider.java
@@ -1,11 +1,14 @@
 package at.uibk.dps.rm.service;
 
+import at.uibk.dps.rm.service.rxjava3.database.log.ReservationLogService;
+import at.uibk.dps.rm.service.rxjava3.database.reservation.ResourceReservationStatusService;
 import at.uibk.dps.rm.service.rxjava3.database.account.AccountCredentialsService;
 import at.uibk.dps.rm.service.rxjava3.database.account.AccountService;
 import at.uibk.dps.rm.service.rxjava3.database.account.CredentialsService;
 import at.uibk.dps.rm.service.rxjava3.database.function.FunctionResourceService;
 import at.uibk.dps.rm.service.rxjava3.database.function.FunctionService;
 import at.uibk.dps.rm.service.rxjava3.database.function.RuntimeService;
+import at.uibk.dps.rm.service.rxjava3.database.log.LogService;
 import at.uibk.dps.rm.service.rxjava3.database.resourceprovider.RegionService;
 import at.uibk.dps.rm.service.rxjava3.database.resourceprovider.ResourceProviderService;
 import at.uibk.dps.rm.service.rxjava3.database.reservation.ReservationService;
@@ -22,45 +25,51 @@ import lombok.Getter;
 
 @Getter
 public class ServiceProxyProvider {
-    private final AccountCredentialsService accountCredentialsService;
     private final AccountService accountService;
-    private final ResourceProviderService resourceProviderService;
-    private final RegionService regionService;
+    private final AccountCredentialsService accountCredentialsService;
     private final CredentialsService credentialsService;
-    private final ResourceService resourceService;
-    private final ResourceTypeService resourceTypeService;
-    private final MetricService metricService;
-    private final MetricValueService metricValueService;
-    private final MetricTypeService metricTypeService;
-    private final RuntimeService runtimeService;
     private final FunctionService functionService;
     private final FunctionResourceService functionResourceService;
+    private final LogService logService;
+    private final MetricService metricService;
+    private final MetricTypeService metricTypeService;
+    private final MetricValueService metricValueService;
+    private final RegionService regionService;
     private final ReservationService reservationService;
+    private final ReservationLogService reservationLogService;
+    private final ResourceService resourceService;
+    private final ResourceProviderService resourceProviderService;
     private final ResourceReservationService resourceReservationService;
+    private final ResourceReservationStatusService resourceReservationStatusService;
+    private final ResourceTypeService resourceTypeService;
+    private final RuntimeService runtimeService;
     private final DeploymentService deploymentService;
-
     private final FilePathService filePathService;
 
     public ServiceProxyProvider(Vertx vertx) {
+        accountService = AccountService.createProxy(vertx, "account-service-address");
         accountCredentialsService = AccountCredentialsService
             .createProxy(vertx, "account-credentials-service-address");
-        accountService = AccountService.createProxy(vertx, "account-service-address");
-        resourceProviderService = ResourceProviderService.createProxy(
-            vertx, "resource-provider-service-address");
-        regionService = RegionService.createProxy(vertx, "region-service-address");
         credentialsService = CredentialsService.createProxy(vertx, "credentials-service-address");
-        resourceService = ResourceService.createProxy(vertx,"resource-service-address");
-        resourceTypeService = ResourceTypeService.createProxy(vertx,"resource-type-service-address");
-        metricService = MetricService.createProxy(vertx,"metric-service-address");
-        metricValueService = MetricValueService.createProxy(vertx,"metric-value-service-address");
-        metricTypeService = MetricTypeService.createProxy(vertx, "metric-type-service-address");
-        runtimeService = RuntimeService.createProxy(vertx, "runtime-service-address");
         functionService = FunctionService.createProxy(vertx, "function-service-address");
         functionResourceService = FunctionResourceService.createProxy(
             vertx, "function-resource-service-address");
+        logService = LogService.createProxy(vertx, "log-service-address");
+        metricService = MetricService.createProxy(vertx,"metric-service-address");
+        metricTypeService = MetricTypeService.createProxy(vertx, "metric-type-service-address");
+        metricValueService = MetricValueService.createProxy(vertx,"metric-value-service-address");
+        regionService = RegionService.createProxy(vertx, "region-service-address");
         reservationService = ReservationService.createProxy(vertx, "reservation-service-address");
+        reservationLogService = ReservationLogService.createProxy(vertx, "reservation-log-service-address");
+        resourceService = ResourceService.createProxy(vertx,"resource-service-address");
+        resourceProviderService = ResourceProviderService.createProxy(
+            vertx, "resource-provider-service-address");
         resourceReservationService = ResourceReservationService
-                .createProxy(vertx, "resource-reservation-service-address");
+            .createProxy(vertx, "resource-reservation-service-address");
+        resourceReservationStatusService = ResourceReservationStatusService
+            .createProxy(vertx, "resource-reservation-status-service-address");
+        resourceTypeService = ResourceTypeService.createProxy(vertx,"resource-type-service-address");
+        runtimeService = RuntimeService.createProxy(vertx, "runtime-service-address");
         deploymentService = DeploymentService.createProxy(vertx, "deployment-service-address");
         filePathService = FilePathService.createProxy(vertx, "file-path-service-address");
     }
--- backend/src/main/java/at/uibk/dps/rm/verticle/DatabaseVerticle.java
+++ backend/src/main/java/at/uibk/dps/rm/verticle/DatabaseVerticle.java
@@ -6,26 +6,30 @@ import at.uibk.dps.rm.repository.account.CredentialsRepository;
 import at.uibk.dps.rm.repository.function.FunctionRepository;
 import at.uibk.dps.rm.repository.function.FunctionResourceRepository;
 import at.uibk.dps.rm.repository.function.RuntimeRepository;
+import at.uibk.dps.rm.repository.log.LogRepository;
+import at.uibk.dps.rm.repository.log.ReservationLogRepository;
 import at.uibk.dps.rm.repository.metric.MetricRepository;
 import at.uibk.dps.rm.repository.metric.MetricTypeRepository;
 import at.uibk.dps.rm.repository.metric.MetricValueRepository;
 import at.uibk.dps.rm.repository.reservation.ReservationRepository;
 import at.uibk.dps.rm.repository.reservation.ResourceReservationRepository;
+import at.uibk.dps.rm.repository.reservation.ResourceReservationStatusRepository;
 import at.uibk.dps.rm.repository.resource.ResourceRepository;
 import at.uibk.dps.rm.repository.resource.ResourceTypeRepository;
 import at.uibk.dps.rm.repository.resourceprovider.RegionRepository;
 import at.uibk.dps.rm.repository.resourceprovider.ResourceProviderRepository;
 import at.uibk.dps.rm.service.database.account.*;
 import at.uibk.dps.rm.service.database.function.*;
+import at.uibk.dps.rm.service.database.log.LogService;
+import at.uibk.dps.rm.service.database.log.LogServiceImpl;
+import at.uibk.dps.rm.service.database.log.ReservationLogService;
+import at.uibk.dps.rm.service.database.log.ReservationLogServiceImpl;
+import at.uibk.dps.rm.service.database.reservation.*;
 import at.uibk.dps.rm.service.database.resourceprovider.RegionService;
 import at.uibk.dps.rm.service.database.resourceprovider.RegionServiceImpl;
 import at.uibk.dps.rm.service.database.resourceprovider.ResourceProviderService;
 import at.uibk.dps.rm.service.database.resourceprovider.ResourceProviderServiceImpl;
 import at.uibk.dps.rm.service.database.metric.*;
-import at.uibk.dps.rm.service.database.reservation.ReservationService;
-import at.uibk.dps.rm.service.database.reservation.ReservationServiceImpl;
-import at.uibk.dps.rm.service.database.reservation.ResourceReservationService;
-import at.uibk.dps.rm.service.database.reservation.ResourceReservationServiceImpl;
 import at.uibk.dps.rm.service.database.resource.ResourceService;
 import at.uibk.dps.rm.service.database.resource.ResourceServiceImpl;
 import at.uibk.dps.rm.service.database.resource.ResourceTypeService;
@@ -48,21 +52,24 @@ public class DatabaseVerticle extends AbstractVerticle {
 
     private SessionFactory sessionFactory;
 
-    private AccountCredentialsRepository accountCredentialsRepository;
     private AccountRepository accountRepository;
-    private ResourceProviderRepository resourceProviderRepository;
-    private RegionRepository regionRepository;
+    private AccountCredentialsRepository accountCredentialsRepository;
     private CredentialsRepository credentialsRepository;
-    private ResourceTypeRepository resourceTypeRepository;
-    private ResourceRepository resourceRepository;
-    private MetricRepository metricRepository;
-    private MetricValueRepository metricValueRepository;
-    private MetricTypeRepository metricTypeRepository;
-    private RuntimeRepository runtimeRepository;
     private FunctionRepository functionRepository;
     private FunctionResourceRepository functionResourceRepository;
+    private LogRepository logRepository;
+    private MetricRepository metricRepository;
+    private MetricTypeRepository metricTypeRepository;
+    private MetricValueRepository metricValueRepository;
+    private RegionRepository regionRepository;
     private ReservationRepository reservationRepository;
+    private ReservationLogRepository reservationLogRepository;
+    private ResourceRepository resourceRepository;
+    private ResourceProviderRepository resourceProviderRepository;
     private ResourceReservationRepository resourceReservationRepository;
+    private ResourceReservationStatusRepository resourceReservationStatusRepository;
+    private ResourceTypeRepository resourceTypeRepository;
+    private RuntimeRepository runtimeRepository;
 
     @Override
     public Completable rxStart() {
@@ -92,88 +99,48 @@ public class DatabaseVerticle extends AbstractVerticle {
 
     private Completable initializeRepositories() {
         Maybe<Void> setupServices = Maybe.create(emitter -> {
-                accountCredentialsRepository = new AccountCredentialsRepository(sessionFactory);
-                accountRepository = new AccountRepository(sessionFactory);
-                resourceProviderRepository = new ResourceProviderRepository(sessionFactory);
-                regionRepository = new RegionRepository(sessionFactory);
-                credentialsRepository = new CredentialsRepository(sessionFactory);
-                resourceTypeRepository = new ResourceTypeRepository(sessionFactory);
-                resourceRepository = new ResourceRepository(sessionFactory);
-                metricRepository = new MetricRepository(sessionFactory);
-                metricValueRepository = new MetricValueRepository(sessionFactory);
-                metricTypeRepository = new MetricTypeRepository(sessionFactory);
-                runtimeRepository = new RuntimeRepository(sessionFactory);
-                functionRepository = new FunctionRepository(sessionFactory);
-                functionResourceRepository = new FunctionResourceRepository(sessionFactory);
-                reservationRepository = new ReservationRepository(sessionFactory);
-                resourceReservationRepository = new ResourceReservationRepository(sessionFactory);
-                emitter.onComplete();
-            }
-        );
+            accountRepository = new AccountRepository(sessionFactory);
+            accountCredentialsRepository = new AccountCredentialsRepository(sessionFactory);
+            credentialsRepository = new CredentialsRepository(sessionFactory);
+            functionRepository = new FunctionRepository(sessionFactory);
+            functionResourceRepository = new FunctionResourceRepository(sessionFactory);
+            logRepository = new LogRepository(sessionFactory);
+            metricRepository = new MetricRepository(sessionFactory);
+            metricTypeRepository = new MetricTypeRepository(sessionFactory);
+            metricValueRepository = new MetricValueRepository(sessionFactory);
+            regionRepository = new RegionRepository(sessionFactory);
+            reservationRepository = new ReservationRepository(sessionFactory);
+            reservationLogRepository = new ReservationLogRepository(sessionFactory);
+            resourceRepository = new ResourceRepository(sessionFactory);
+            resourceProviderRepository = new ResourceProviderRepository(sessionFactory);
+            resourceReservationRepository = new ResourceReservationRepository(sessionFactory);
+            resourceReservationStatusRepository = new ResourceReservationStatusRepository(sessionFactory);
+            resourceTypeRepository = new ResourceTypeRepository(sessionFactory);
+            runtimeRepository = new RuntimeRepository(sessionFactory);
+            emitter.onComplete();
+        });
         return Completable.fromMaybe(setupServices);
     }
 
     private Completable setupEventBus() {
         Maybe<Void> setupEventBus = Maybe.create(emitter -> {
-            AccountCredentialsService accountCredentialsService =
-                new AccountCredentialsServiceImpl(accountCredentialsRepository);
-
             ServiceBinder serviceBinder = new ServiceBinder(vertx.getDelegate());
-            serviceBinder
-                .setAddress("account-credentials-service-address")
-                .register(AccountCredentialsService.class, accountCredentialsService);
 
             AccountService accountService = new AccountServiceImpl(accountRepository);
             serviceBinder
                 .setAddress("account-service-address")
                 .register(AccountService.class, accountService);
 
-            ResourceProviderService resourceProviderService =
-                new ResourceProviderServiceImpl(resourceProviderRepository);
+            AccountCredentialsService accountCredentialsService =
+                new AccountCredentialsServiceImpl(accountCredentialsRepository);
             serviceBinder
-                .setAddress("resource-provider-service-address")
-                .register(ResourceProviderService.class, resourceProviderService);
-
-            RegionService regionService =
-                new RegionServiceImpl(regionRepository);
-            serviceBinder
-                .setAddress("region-service-address")
-                .register(RegionService.class, regionService);
+                .setAddress("account-credentials-service-address")
+                .register(AccountCredentialsService.class, accountCredentialsService);
 
             CredentialsService credentialsService = new CredentialsServiceImpl(credentialsRepository);
             serviceBinder
                 .setAddress("credentials-service-address")
                 .register(CredentialsService.class, credentialsService);
-
-            ResourceTypeService resourceTypeService = new ResourceTypeServiceImpl(resourceTypeRepository);
-            serviceBinder
-                .setAddress("resource-type-service-address")
-                .register(ResourceTypeService.class, resourceTypeService);
-
-            ResourceService resourceService = new ResourceServiceImpl(resourceRepository);
-            serviceBinder
-                .setAddress("resource-service-address")
-                .register(ResourceService.class, resourceService);
-
-            MetricService metricService = new MetricServiceImpl(metricRepository);
-            serviceBinder
-                .setAddress("metric-service-address")
-                .register(MetricService.class, metricService);
-
-            MetricValueService metricValueService = new MetricValueServiceImpl(metricValueRepository);
-            serviceBinder
-                    .setAddress("metric-value-service-address")
-                    .register(MetricValueService.class, metricValueService);
-
-            MetricTypeService metricTypeService = new MetricTypeServiceImpl(metricTypeRepository);
-            serviceBinder
-                    .setAddress("metric-type-service-address")
-                    .register(MetricTypeService.class, metricTypeService);
-
-            RuntimeService runtimeService = new RuntimeServiceImpl(runtimeRepository);
-            serviceBinder
-                .setAddress("runtime-service-address")
-                .register(RuntimeService.class, runtimeService);
 
             FunctionService functionService = new FunctionServiceImpl(functionRepository);
             serviceBinder
@@ -186,16 +153,75 @@ public class DatabaseVerticle extends AbstractVerticle {
                 .setAddress("function-resource-service-address")
                 .register(FunctionResourceService.class, functionResourceService);
 
+            LogService logService =
+                new LogServiceImpl(logRepository);
+            serviceBinder
+                .setAddress("log-service-address")
+                .register(LogService.class, logService);
+
+            MetricService metricService = new MetricServiceImpl(metricRepository);
+            serviceBinder
+                .setAddress("metric-service-address")
+                .register(MetricService.class, metricService);
+
+            MetricTypeService metricTypeService = new MetricTypeServiceImpl(metricTypeRepository);
+            serviceBinder
+                .setAddress("metric-type-service-address")
+                .register(MetricTypeService.class, metricTypeService);
+
+            MetricValueService metricValueService = new MetricValueServiceImpl(metricValueRepository);
+            serviceBinder
+                .setAddress("metric-value-service-address")
+                .register(MetricValueService.class, metricValueService);
+
+            RegionService regionService =
+                new RegionServiceImpl(regionRepository);
+            serviceBinder
+                .setAddress("region-service-address")
+                .register(RegionService.class, regionService);
+
             ReservationService reservationService = new ReservationServiceImpl(reservationRepository);
             serviceBinder
-                    .setAddress("reservation-service-address")
-                    .register(ReservationService.class, reservationService);
+                .setAddress("reservation-service-address")
+                .register(ReservationService.class, reservationService);
+
+            ReservationLogService reservationLogService = new ReservationLogServiceImpl(reservationLogRepository);
+            serviceBinder
+                .setAddress("reservation-log-service-address")
+                .register(ReservationLogService.class, reservationLogService);
+
+            ResourceService resourceService = new ResourceServiceImpl(resourceRepository);
+            serviceBinder
+                .setAddress("resource-service-address")
+                .register(ResourceService.class, resourceService);
+
+            ResourceProviderService resourceProviderService =
+                new ResourceProviderServiceImpl(resourceProviderRepository);
+            serviceBinder
+                .setAddress("resource-provider-service-address")
+                .register(ResourceProviderService.class, resourceProviderService);
 
             ResourceReservationService resourceReservationService =
                 new ResourceReservationServiceImpl(resourceReservationRepository);
             serviceBinder
-                    .setAddress("resource-reservation-service-address")
-                    .register(ResourceReservationService.class, resourceReservationService);
+                .setAddress("resource-reservation-service-address")
+                .register(ResourceReservationService.class, resourceReservationService);
+
+            ResourceReservationStatusService resourceReservationStatusService =
+                new ResourceReservationStatusServiceImpl(resourceReservationStatusRepository);
+            serviceBinder
+                .setAddress("resource-reservation-status-service-address")
+                .register(ResourceReservationStatusService.class, resourceReservationStatusService);
+
+            ResourceTypeService resourceTypeService = new ResourceTypeServiceImpl(resourceTypeRepository);
+            serviceBinder
+                .setAddress("resource-type-service-address")
+                .register(ResourceTypeService.class, resourceTypeService);
+
+            RuntimeService runtimeService = new RuntimeServiceImpl(runtimeRepository);
+            serviceBinder
+                .setAddress("runtime-service-address")
+                .register(RuntimeService.class, runtimeService);
 
             FilePathService filePathService =
                 new FilePathServiceImpl(vertx.getDelegate());
