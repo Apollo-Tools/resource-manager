7337a0a9a5d5a887c0155bed72f5b0593522aaac
matthi-g <matthias.gassner@student.uibk.ac.at>
2023-03-22 21:23:20 +0100

    Unify return objects for lambda and openfaas

--- backend/src/main/resources/faas/python/handler.py
+++ backend/src/main/resources/faas/python/handler.py
@@ -3,7 +3,6 @@ from .cloud_function import main
 
 # OpenFaaS
 def handle(req):
-    print(req)
     input_body = json.loads(req)
     res = main(input_body)
-    return {"statusCode": 200, "body": res}
+    return res
