47abcc5914639fe34344a33f63762b61b53eb0dc
matthi-g <matthias.gassner@student.uibk.ac.at>
2023-05-20 00:36:47 +0200

    Cleanup

--- backend/terraform/aws/vm/templates/startup.sh
+++ backend/terraform/aws/vm/templates/startup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 mkdir -p /var/lib/faasd/secrets/
-echo ${basic_auth_user} > /var/lib/faasd/secrets/basic-auth-user
-echo ${basic_auth_password} > /var/lib/faasd/secrets/basic-auth-password
+echo ${basic_auth_user} > "/var/lib/faasd/secrets/basic-auth-user"
+echo ${basic_auth_password} > "/var/lib/faasd/secrets/basic-auth-password"
 
 curl -sfL https://raw.githubusercontent.com/openfaas/faasd/master/hack/install.sh | bash -s -
