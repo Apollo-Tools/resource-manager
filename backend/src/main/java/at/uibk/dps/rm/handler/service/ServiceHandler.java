230bd0fb23789ba0c254684890ee4f2cdd3aed40
matthi-g <matthias.gassner@student.uibk.ac.at>
2023-05-20 04:38:04 +0200

    Implement check for service type for service creation

--- backend/src/main/java/at/uibk/dps/rm/handler/service/ServiceHandler.java
+++ backend/src/main/java/at/uibk/dps/rm/handler/service/ServiceHandler.java
@@ -1,6 +1,9 @@
 package at.uibk.dps.rm.handler.service;
 
 import at.uibk.dps.rm.handler.ValidationHandler;
+import io.reactivex.rxjava3.core.Single;
+import io.vertx.core.json.JsonObject;
+import io.vertx.rxjava3.ext.web.RoutingContext;
 
 /**
  * Processes the http requests that concern the service entity.
@@ -8,12 +11,27 @@ import at.uibk.dps.rm.handler.ValidationHandler;
  * @author matthi-g
  */
 public class ServiceHandler extends ValidationHandler {
+
+    private final ServiceTypeChecker serviceTypeChecker;
     /**
-     * Create an instance from the serviceChecker.
+     * Create an instance from the serviceChecker and serviceTypeChecker.
      *
      * @param serviceChecker the service checker
+     * @param serviceTypeChecker the service type checker
      */
-    public ServiceHandler(ServiceChecker serviceChecker) {
+    public ServiceHandler(ServiceChecker serviceChecker, ServiceTypeChecker serviceTypeChecker) {
         super(serviceChecker);
+        this.serviceTypeChecker = serviceTypeChecker;
+    }
+
+    @Override
+    public Single<JsonObject> postOne(RoutingContext rc) {
+        JsonObject requestBody = rc.body().asJsonObject();
+        return serviceTypeChecker.checkExistsOne(requestBody
+                .getJsonObject("service_type")
+                .getLong("service_type_id"))
+            .andThen(entityChecker.checkForDuplicateEntity(requestBody))
+            .andThen(Single.defer(() -> Single.just(1L)))
+            .flatMap(result -> entityChecker.submitCreate(requestBody));
     }
 }
--- backend/src/main/java/at/uibk/dps/rm/router/service/ServiceRoute.java
+++ backend/src/main/java/at/uibk/dps/rm/router/service/ServiceRoute.java
@@ -3,6 +3,7 @@ package at.uibk.dps.rm.router.service;
 import at.uibk.dps.rm.handler.ResultHandler;
 import at.uibk.dps.rm.handler.service.ServiceChecker;
 import at.uibk.dps.rm.handler.service.ServiceHandler;
+import at.uibk.dps.rm.handler.service.ServiceTypeChecker;
 import at.uibk.dps.rm.router.Route;
 import at.uibk.dps.rm.service.ServiceProxyProvider;
 import io.vertx.rxjava3.ext.web.openapi.RouterBuilder;
@@ -16,7 +17,8 @@ public class ServiceRoute implements Route {
     @Override
     public void init(RouterBuilder router, ServiceProxyProvider serviceProxyProvider) {
         ServiceChecker serviceChecker = new ServiceChecker(serviceProxyProvider.getServiceService());
-        ServiceHandler serviceHandler = new ServiceHandler(serviceChecker);
+        ServiceTypeChecker serviceTypeChecker = new ServiceTypeChecker(serviceProxyProvider.getServiceTypeService());
+        ServiceHandler serviceHandler = new ServiceHandler(serviceChecker, serviceTypeChecker);
         ResultHandler resultHandler = new ResultHandler(serviceHandler);
 
         router
