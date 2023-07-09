c97fbf585a3dab6b64946e6d16f0161cd7b2abb8
matthi-g <matthias.gassner@student.uibk.ac.at>
2023-07-09 04:37:38 +0200

    Update python template with import

--- backend/faas-templates/python38/apollorm/main.py
+++ backend/faas-templates/python38/apollorm/main.py
@@ -1,11 +1,18 @@
+# Import from other files https://stackoverflow.com/a/49480246
+if __package__ is None or __package__ == '':
+    from subscript import sum
+else:
+    from .subscript import sum
+
 #########Function implementation#############
 def main(json_input):
     input1 = json_input["input1"]
 
     # Processing
+    sum_result = sum(input1, 10)
 
     # return the result
     res = {
-        "input1": input1
+        "input1": sum_result
     }
     return res
