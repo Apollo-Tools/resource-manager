3ecfe9f422c055a1c17d115f031c496133e733dd
matthi-g <matthias.gassner@student.uibk.ac.at>
2023-07-10 00:47:34 +0200

    Add java doc

--- backend/src/main/java/at/uibk/dps/rm/util/misc/UploadFileHelper.java
+++ backend/src/main/java/at/uibk/dps/rm/util/misc/UploadFileHelper.java
@@ -7,9 +7,21 @@ import lombok.experimental.UtilityClass;
 
 import java.nio.file.Path;
 
+/**
+ * This class is used to persist, delete or update persisted file uploads.
+ *
+ * @author matthi-g
+ */
 @UtilityClass
 public class UploadFileHelper {
 
+    /**
+     * Move an uploaded file from the temp directory to the persist directory.
+     *
+     * @param vertx the vertx instance
+     * @param fileName the name of the file
+     * @return a Completable
+     */
     public static Completable persistUploadedFile(Vertx vertx, String fileName) {
         return new ConfigUtility(vertx).getConfig()
             .flatMapCompletable(config -> {
@@ -19,6 +31,13 @@ public class UploadFileHelper {
             });
     }
 
+    /**
+     * Delete a persisted file.
+     *
+     * @param vertx the vertx instance
+     * @param fileName the name of the file
+     * @return a Completable
+     */
     public static Completable deleteFile(Vertx vertx, String fileName) {
         return new ConfigUtility(vertx).getConfig().flatMapCompletable(config -> {
             Path filePath = Path.of(config.getString("upload_persist_directory"), fileName);
@@ -34,6 +53,14 @@ public class UploadFileHelper {
         });
     }
 
+    /**
+     * Delete the old file and replace it with the new one.
+     *
+     * @param vertx the vertx instance
+     * @param oldFileName the name of the old file
+     * @param newFileName the name of the new file
+     * @return a Completable
+     */
     public static Completable updateFile(Vertx vertx, String oldFileName, String newFileName) {
         return deleteFile(vertx, oldFileName)
             .andThen(persistUploadedFile(vertx, newFileName));
