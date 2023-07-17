dab337bdcecd3d1241e5f11bbef1fc4f48ae4fdd
matthi-g <matthias.gassner@student.uibk.ac.at>
2023-07-18 01:37:29 +0200

    Update input handler for create deployment operation

--- backend/src/main/java/at/uibk/dps/rm/entity/dto/credentials/DockerCredentials.java
+++ backend/src/main/java/at/uibk/dps/rm/entity/dto/credentials/DockerCredentials.java
@@ -9,7 +9,22 @@ import lombok.Data;
  */
 @Data
 public class DockerCredentials {
-    private String registry;
+    private String registry = "";
     private String username;
     private String accessToken;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DockerCredentials that = (DockerCredentials) o;
+
+        return registry.equals(that.registry);
+    }
+
+    @Override
+    public int hashCode() {
+        return registry.hashCode();
+    }
 }
