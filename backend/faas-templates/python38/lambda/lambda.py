326840ab4f60178af93762f77114adc5f7e34cd2
matthi-g <matthias.gassner@student.uibk.ac.at>
2023-10-28 20:32:58 +0200

    Add time measurements to lambda python code wrapper

--- backend/faas-templates/python38/lambda/lambda.py
+++ backend/faas-templates/python38/lambda/lambda.py
@@ -1,13 +1,33 @@
 import json
+import time
+from enum import Enum
 from main import main
+
+class RequestType(str, Enum):
+    CLIENT = 'client'
+    RM = 'rm'
+
 
 ########## Boilerplate wrapping code #############
 def handler(event, context):
+    start = time.time()
+    add_monitoring_data = False
+    # check request type
+    headers = event['headers']
+    if 'apollo-request-type' in headers and headers['apollo-request-type'] == RequestType.RM:
+        add_monitoring_data = True
     # read in the args from the POST object
     if 'body' in event and isinstance(event['body'], str):
         input_body = json.loads(event['body'])
     else:
         input_body = event
-    res = main(input_body)
-    return {"statusCode": 200, "body": res}
+    # execute function
+    body = main(input_body)
+    # return result
+    if add_monitoring_data:
+        end = time.time()
+        body = {"monitoring_data": {"execution_time_ms": (end-start) * 1000, "start_timestamp": int(start * 1000)},
+                "body": body}
+    return {"statusCode": 200, "body": body}
 ##################################################
+
