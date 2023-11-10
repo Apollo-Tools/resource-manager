9d71f60f31f549bb8a3f846c530b19696e498388
matthi-g <matthias.gassner@student.uibk.ac.at>
2023-11-10 20:30:09 +0100

    Add MonitoringHandler interface

---
+++ backend/src/main/java/at/uibk/dps/rm/handler/monitoring/K8sMonitoringHandler.java
@@ -0,0 +1,124 @@
+package at.uibk.dps.rm.handler.monitoring;
+
+import at.uibk.dps.rm.entity.dto.config.ConfigDTO;
+import at.uibk.dps.rm.entity.monitoring.K8sMonitoringData;
+import at.uibk.dps.rm.entity.monitoring.K8sNode;
+import at.uibk.dps.rm.exception.MonitoringException;
+import at.uibk.dps.rm.service.ServiceProxyProvider;
+import at.uibk.dps.rm.service.monitoring.k8s.K8sMonitoringService;
+import at.uibk.dps.rm.service.monitoring.k8s.K8sMonitoringServiceImpl;
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.models.V1Namespace;
+import io.kubernetes.client.util.Config;
+import io.reactivex.rxjava3.core.Observable;
+import io.vertx.core.Handler;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+import io.vertx.rxjava3.core.Vertx;
+import io.vertx.rxjava3.core.buffer.Buffer;
+import io.vertx.rxjava3.core.file.FileSystem;
+import lombok.RequiredArgsConstructor;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * This monitoring handler monitors k8s clusters. This includes checking if new nodes got added,
+ * existing nodes got deleted and the current usage of cpu, memory and storage of each node.
+ *
+ */
+@RequiredArgsConstructor
+public class K8sMonitoringHandler implements MonitoringHandler {
+    private static final Logger logger = LoggerFactory.getLogger(K8sMonitoringHandler.class);
+
+    private final Vertx vertx;
+    
+    private final ConfigDTO configDTO;
+
+    private long currentTimer = -1L;
+
+    private boolean pauseLoop = false;
+
+    private static Handler<Long> monitoringHandler;
+    
+    @Override
+    public void startMonitoringLoop() {
+        pauseLoop = false;
+        long period = (long) (configDTO.getKubeMonitoringPeriod() * 60 * 1000);
+        ServiceProxyProvider serviceProxyProvider = new ServiceProxyProvider(vertx);
+        monitoringHandler = id -> {
+            logger.info("Started: monitor k8s resources");
+            vertx.executeBlocking(fut -> fut.complete(monitorK8s()))
+                .map(monitoringData -> (Map<String, K8sMonitoringData>) monitoringData)
+                .flatMapObservable(monitoringData -> Observable.fromIterable(monitoringData.entrySet()))
+                .flatMapCompletable(entry -> {
+                    List<String> namespaces = entry.getValue().getNamespaces().stream()
+                        .map(namespace -> Objects.requireNonNull(namespace.getMetadata()).getName())
+                        .collect(Collectors.toList());
+                    return serviceProxyProvider.getResourceService()
+                        .updateClusterResource(entry.getKey(), entry.getValue())
+                        .andThen(serviceProxyProvider.getNamespaceService().updateAllClusterNamespaces(entry.getKey(),
+                            namespaces))
+                        .doOnError(throwable -> {
+                            logger.error(throwable.getMessage());
+                            if (!(throwable instanceof MonitoringException)) {
+                                throw new RuntimeException(throwable);
+                            }
+                        });
+                }).subscribe(() -> {
+                    logger.info("Finished: monitor k8s resources");
+                    currentTimer = pauseLoop ? currentTimer : vertx.setTimer(period, monitoringHandler);
+                }, throwable -> {
+                    logger.error(throwable.getMessage());
+                    currentTimer = pauseLoop ? currentTimer : vertx.setTimer(period, monitoringHandler);
+                });
+        };
+        currentTimer = vertx.setTimer(period, monitoringHandler);
+    }
+
+    public void pauseMonitoringLoop() {
+        pauseLoop = true;
+        if (!vertx.cancelTimer(currentTimer)) {
+            vertx.cancelTimer(currentTimer);
+        }
+        currentTimer = -1L;
+    }
+
+    // TODO: parallelize monitoring of multiple clusters, retrieve configs from k8s secret volume
+    private Map<String, K8sMonitoringData> monitorK8s() {
+        try {
+            FileSystem fileSystem = vertx.fileSystem();
+            Map<String, K8sMonitoringData> monitoringDataMap = new HashMap<>();
+            K8sMonitoringService monitoringService = new K8sMonitoringServiceImpl();
+            Map<String, String> kubeConfigs = monitoringService.listSecrets(configDTO);
+            if (!fileSystem.existsBlocking(configDTO.getKubeConfigDirectory())) {
+                fileSystem.mkdirsBlocking(configDTO.getKubeConfigDirectory());
+            }
+            vertx.fileSystem().mkdirsBlocking(configDTO.getKubeConfigDirectory());
+            for (Map.Entry<String, String> entry: kubeConfigs.entrySet()) {
+                logger.info("Observe cluster: " + entry.getKey());
+                Path kubeconfigPath = Path.of(configDTO.getKubeConfigDirectory(), entry.getKey());
+                fileSystem.writeFileBlocking(kubeconfigPath.toString(), Buffer.buffer(entry.getValue()));
+                ApiClient externalClient = Config.fromConfig(new StringReader(entry.getValue()));
+                Configuration.setDefaultApiClient(externalClient);
+                List<K8sNode> nodes = monitoringService.listNodes(entry.getValue(), configDTO);
+                List<V1Namespace> namespaces = monitoringService.listNamespaces(entry.getValue(), configDTO);
+                for (K8sNode node: nodes) {
+                    monitoringService.getCurrentNodeAllocation(node, entry.getValue(), configDTO);
+                }
+                K8sMonitoringData k8sMonitoringData = new K8sMonitoringData(nodes, namespaces);
+                monitoringDataMap.put(entry.getKey(), k8sMonitoringData);
+            }
+            return monitoringDataMap;
+        } catch (IOException e) {
+            throw new MonitoringException("failed to setup config");
+        }
+    }
+}
